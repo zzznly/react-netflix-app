d752e8b7f2c826b34932d3bda42f0abfd511c84c
Jinri <zzznly@gmail.com>
2022-11-06 23:38:16 +0900

    feat. useDebounce 적용

--- src/pages/SearchPage/index.js
+++ src/pages/SearchPage/index.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from "react";
 import {useLocation} from "react-router-dom";
 import axios from "../../api/axios";
 import './SearchPage.css'
+import {useDebounce} from "../../hooks/useDebounce";
 
 export default function SearchPage() {
     // console.log('useLocation', useLocation());
@@ -12,15 +13,17 @@ export default function SearchPage() {
     };
     let query = useQuery();
     const searchTerm = query.get("q");
+    const debouncedSearchTerm = useDebounce(searchTerm, 500)
 
     // searchTerm이 바뀔때마다 새로 영화 데이터 가져오기
     useEffect(() => {
-        if (searchTerm) {
-            fetchSearchMovie(searchTerm);
+        if (debouncedSearchTerm) {
+            fetchSearchMovie(debouncedSearchTerm);
         }
-    }, [searchTerm]);
+    }, [debouncedSearchTerm]);
 
     const fetchSearchMovie = async (searchTerm) => {
+        console.log('searchTerm', searchTerm);
         try {
             const request = await axios.get(
                 `search/multi?include_adult=false&query=${searchTerm}`
@@ -39,7 +42,7 @@ export default function SearchPage() {
                         if(movie.backdrop_path !== null && movie.media_type !== "person") {
                             const movieImageUrl = "https://image.tmdb.org/t/p/w500" + movie.backdrop_path;
                             return (
-                                <div className="movie">
+                                <div className="movie" key={movie.id}>
                                     <div className="movie__column-poster">
                                         <img src={movieImageUrl} alt="" className="movie__poster"/>
                                     </div>
@@ -50,7 +53,7 @@ export default function SearchPage() {
                 }
             </section>) :
             (<section className="no-results">
-                <p>Your search for "{searchTerm}" did not have any matches.</p>
+                <p>Your search for "{debouncedSearchTerm}" did not have any matches.</p>
             </section>)
     };
 
